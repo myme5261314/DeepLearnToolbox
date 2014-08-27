b0e7652e2602da521e8846b11d952e1190ea3793
Peng Liu <myme5261314@gmail.com>
2014-08-27 16:46:25 +0800

    Batch experiment for AsyRBM with thread_num=1-20, n_fetch=10, n_push=10.

--- tests/AsyRBM_Exp.m
+++ tests/AsyRBM_Exp.m
@@ -1,24 +1,39 @@
-error_list = zeros(20, 10);
+diary('log.txt');
+diary on;
+disp('v0.06');
+
+% error_list = zeros(20, 10);
 asy_error_list = zeros(20, 10);
 
+n_fetch = 10;
+n_push = 20;
+
+tic;
+
 %% Normal RBM split dataset performance.
-for i=1:size(error_list,1)
-    for j=1:size(error_list,2)
-        error_list(i,j) = test_example_DBN(false, i, 1, 1);
-    end
-end
+% for i=1:size(error_list,1)
+%     for j=1:size(error_list,2)
+%         error_list(i,j) = test_example_DBN(false, i, 1, 1);
+%     end
+% end
 
 %% Asy RBM thread_num performance.
 for i=1:size(asy_error_list,1)
     for j=1:size(asy_error_list,2)
-        asy_error_list(i,j) = test_example_DBN(true, i, 10, 5);
+        asy_error_list(i,j) = test_example_DBN(true, i, n_fetch, n_push);
     end
 end
 
-rbm_accuracy = 1-mean(error_list);
-asy_rbm_accuracy = 1-mean(asy_error_list);
+toc;
+
+save('metrics.mat', 'asy_error_list');
+
+% rbm_accuracy = 1-mean(error_list,2);
+asy_rbm_accuracy = 1-mean(asy_error_list,2);
 
 figure;
-plot(1:size(error_list,1), rbm_accuracy, 'red');
-hold all;
+% plot(1:size(error_list,1), rbm_accuracy, 'red');
+% hold all;
 plot(1:size(asy_error_list,1), asy_rbm_accuracy, 'blue');
+
+diary off;
--- tests/test_example_DBN.m
+++ tests/test_example_DBN.m
@@ -17,16 +17,16 @@ test_y  = double(test_y);
 
 %%  ex1 train a 100 hidden unit RBM and visualize its weights
 % rand('state',0)
-dbn.sizes = [100];
-opts.numepochs =   1;
-opts.batchsize = 100;
-opts.momentum  =   0;
-opts.alpha     =   1;
+% dbn.sizes = [100];
+% opts.numepochs =   1;
+% opts.batchsize = 100;
+% opts.momentum  =   0;
+% opts.alpha     =   1;
 opts.thread_num = thread_num;
 opts.n_fetch = fetch;
 opts.n_push = push;
-dbn = dbnsetup(dbn, train_x, opts);
-dbn = dbntrain(dbn, train_x, opts);
+% dbn = dbnsetup(dbn, train_x, opts);
+% dbn = dbntrain(dbn, train_x, opts);
 % figure; visualize(dbn.rbm{1}.W');   %  Visualize the RBM weights
 
 %%  ex2 train a 100-100 hidden unit DBN and use its weights to initialize a NN
@@ -37,8 +37,10 @@ opts.numepochs =   10;
 opts.batchsize = 100;
 opts.momentum  =   0;
 opts.alpha     =   1;
+tic;
 dbn = dbnsetup(dbn, train_x, opts);
 dbn = dbntrain(dbn, train_x, opts);
+toc;
 % figure; visualize(dbn.rbm{1}.W');   %  Visualize the RBM weights
 
 %unfold dbn to nn
@@ -48,8 +50,10 @@ nn.activation_function = 'sigm';
 %train nn
 opts.numepochs =  10;
 opts.batchsize = 100;
+tic;
 nn = nntrain(nn, train_x, train_y, opts);
 [er, bad] = nntest(nn, test_x, test_y);
-disp(er);
+toc;
+disp([num2str(er*100), '%']);
 
 % assert(er < 0.10, 'Too big error');
